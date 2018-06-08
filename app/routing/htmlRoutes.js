4705d348665e3f38295f905d8a5dcf3fccd79b03
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-07 18:21:06 -0700

    Converted

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,36 +1,30 @@
 
-const path = require('path');
-const data = require(path.join(__dirname,'../data/friends'));
+var path = require('path');
+var data = require(path.join(__dirname,'../data/friends'));
 
-class HTMLRoutes {
+var htmlRoutes = {
   
-  /**
-   * attaches our routes to the express server
-   * @param  {Object} app is the instance of our express server
-   */
-  attach(app) {
+  attach: function(app) {
     app.get('/view',this.nonSense);
     app.get('/survey',this.sendSurvey);
     app.get('/',this.home);
-  }
+  },
 
-  sendSurvey(req,res) {
-    let { body } = req;
-    console.log(body);
+  sendSurvey: function(req,res) {
+    var body = req.body;
     res.sendFile(path.join(__dirname,'../../app/public/survey.html'));
-  }
+  },
 
-  home(req,res) {
-    let { body } = req;
+  home: function(req,res) {
+    var body = req.body;
     res.sendFile(path.join(__dirname,'../../app/public/home.html'));
-  }
+  },
 
-  nonSense(req,res) {
-    let { body } = req;
-    console.log(body);
+  nonSense: function(req,res) {
+    var body = req.body;
     res.sendFile(path.join(__dirname,'../../app/public/potential-friends.html'));
   }
 
 }
 
-module.exports = new HTMLRoutes();
+module.exports = htmlRoutes;
